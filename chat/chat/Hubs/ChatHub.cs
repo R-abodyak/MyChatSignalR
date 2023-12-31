513ec63b7ded42a8064b00e289ca570cab667e80
R-Abodyak <rawanabodiak12@gmail.com>
2023-12-31 21:57:30 +0200

    Build server side and configration

---
+++ chat/chat/Hubs/ChatHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Server.HttpSys;
+using Microsoft.AspNetCore.SignalR;
+using System.Reflection;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace chat.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+
+        public async Task SendMessage(string message, string sender, string receiverId)
+        {
+
+            await Clients.Client(receiverId).SendAsync("ReceiveMessage", message, sender);
+
+        }
+
+
+        public override async Task OnConnectedAsync()
+        {
+            var clientId = Context.ConnectionId; // Use the connection ID as a unique identifier
+            await Clients.Client(clientId).SendAsync("connected", clientId);
+
+            await base.OnConnectedAsync();
+        }
+    }
+ }
+
--- chat/chat/Program.cs
+++ chat/chat/Program.cs
@@ -1,27 +1,51 @@
-var builder = WebApplication.CreateBuilder(args);
+using chat.Hubs;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.ConfigureServices(services =>
+                {
+                    services.AddSignalR();
+                    services.AddControllersWithViews();
+
+                });
+
+                webBuilder.Configure(app =>
+                {
+                    var env = app.ApplicationServices.GetRequiredService<IWebHostEnvironment>();
+
+                    if (env.IsDevelopment())
+                    {
+                        app.UseDeveloperExceptionPage();
+                    }
+                    else
+                    {
+                        app.UseExceptionHandler("/Home/Error");
+                        app.UseHsts();
+                    }
+
+                    app.UseHttpsRedirection();
+                    app.UseStaticFiles();
+
+                    app.UseRouting();
+
+                    app.UseEndpoints(endpoints =>
+                    {
+                        endpoints.MapControllerRoute(
+                            name: "default",
+                            pattern: "{controller=Home}/{action=Index}/{id?}");
+                        endpoints.MapHub<ChatHub>("/ChatHub"); // Map the SignalR hub
+                    });
+                });
+            });
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
