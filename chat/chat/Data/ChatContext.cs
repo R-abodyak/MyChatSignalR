bfd2962c7c37ba46b521c70babb2f6382de804ba
RawanAbuDyak <rawanabodiak12@gmail.com>
2024-02-17 23:22:47 +0200

    fix db sets

--- chat/chat/Data/ChatContext.cs
+++ chat/chat/Data/ChatContext.cs
@@ -12,5 +12,4 @@ public class ChatContext : DbContext
 
     // DbSet properties for database entities
     public DbSet<User> User { get; set; } = null!;
-    public DbSet<User> User2 { get; set; } = null!;
 }
